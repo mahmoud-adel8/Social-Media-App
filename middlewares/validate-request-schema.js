88ef48227609e938dd4e270c0a9888644198eba0
Mahmoud Adel <mahmoud.adel.abdelfattah238@gmail.com>
2022-10-13 13:43:20 +0200

    fixed a pug related to image uploading

--- middlewares/validate-request-schema.js
+++ middlewares/validate-request-schema.js
@@ -1,8 +1,12 @@
 import { validationResult } from 'express-validator';
+import { clearImage } from '../util/file-helper.js';
 
 export function validateRequestSchema(req, res, next) {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
+    if (req.file) {
+      clearImage(req.file.path);
+    }
     const err = new Error('The date entered is not correct.');
     err.statusCode = 422;
     err.details = errors.array();
