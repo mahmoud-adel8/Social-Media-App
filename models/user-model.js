ddaa71c66e58a64f456afca06d0927edadd5f09d
Mahmoud Adel <mahmoud.adel.abdelfattah238@gmail.com>
2022-10-15 17:14:34 +0200

    added user model

---
+++ models/user-model.js
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  posts: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Post'
+  }]
+});
+
+const UserModel = model('User', userSchema);
+
+export default UserModel;
